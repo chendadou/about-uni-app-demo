3d381aea6076cf4f95f65b0e814f56dd4f788fac
Chen Yanyun <chenyanyun@dadou.local>
2023-03-21 22:23:49 +0800

    demo

---
+++ pages/index/index.vue
@@ -0,0 +1,127 @@
+<template>
+	<view class="content">
+		<input type="text" class="input" v-model="state.content" placeholder="请输入">
+		<button type="primary" @click="publish()" size="mini">发布</button>
+	</view>
+	<view v-for="item in state.list" :key="item._id">
+		<view class="box">
+			<view>{{item.content}}</view>
+			<button type="warn" size="mini" @click="remove(item._id)" class="remove-btn">删除</button>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import {
+		reactive,
+		onMounted
+	} from 'vue';
+
+	const state = reactive({
+		content: '',
+		list: [],
+	});
+
+	onMounted(() => {
+		getMessage();
+		uni.showModal({
+			title: '用户需知',
+			content: '测试一下',
+			success: (confirm, cancel) => {
+				if (confirm) {
+					uni.getSetting({
+						success: res => {
+							console.log('res.authSetting', res.authSetting)
+						}
+					})
+				}
+				if (cancel) {
+
+				}
+			}
+		});
+	})
+
+	function getMessage() {
+		uniCloud.callFunction({
+			name: 'fun',
+			data: {
+				api: 'getMessage',
+			},
+		}).then(res => {
+			state.list = res.result.data.reverse();
+		});
+	}
+
+	function publish() {
+		if (state.content) {
+			uni.showLoading({
+				title: '留言上传中...'
+			});
+			uniCloud.callFunction({
+				name: 'fun',
+				data: {
+					api: 'publish',
+					content: state.content
+				}
+			}).then(res => {
+				console.log('publish', res);
+				getMessage();
+				uni.showToast({
+					title: '留言成功！',
+					icon: 'success'
+				});
+				state.content = '';
+			});
+		} else {
+			uni.showToast({
+				title: '请输入留言内容！',
+				icon: 'none'
+			});
+		}
+	}
+
+	function remove(id) {
+		uni.showLoading({
+			title: '删除中...'
+		});
+		uniCloud.callFunction({
+			name: 'fun',
+			data: {
+				api: 'remove',
+				_id: id
+			}
+		}).then(res => {
+			getMessage();
+			uni.showToast({
+				title: '删除成功！',
+				icon: 'success'
+			});
+			console.log('remove', res);
+		});
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		align-items: center;
+		margin: 40rpx;
+	}
+
+	.input {
+		border-bottom: 1px solid #ccc;
+		padding: 6rpx;
+		flex: 1;
+	}
+
+	.box {
+		margin: 40rpx;
+		border-bottom: 1px solid #ccc;
+		padding: 10rpx 0;
+
+		.remove-btn {
+			margin-top: 10rpx;
+		}
+	}
+</style>
---
+++ uniCloud-aliyun/cloudfunctions/fun/index.js
@@ -0,0 +1,27 @@
+let db = uniCloud.database({
+	throwOnNotFound: false,
+})
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+
+	if (event.api === 'publish') {
+		return await db.collection('message').add({
+			content: event.content,
+			publish: true
+		})
+	}
+
+	if (event.api === 'getMessage') {
+		return await db.collection('message').where({
+			publish: true
+		}).get();
+	}
+
+	if (event.api === 'remove') {
+		return await db.collection('message').doc(event._id).remove();
+	}
+
+	//返回数据给客户端
+	return '新消息提醒'
+};
